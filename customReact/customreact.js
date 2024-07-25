caf973fdf7e47dd1f622cc23c80739a08bd75aec
huzair <huzairahmedkhan@gmail.com>
2024-07-25 17:27:59 +0500

    create elemnt with react create element

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,8 +1,19 @@
 function renderElement (element, container) {
+
+    // creating dom element and setting the props manually
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.child
+    // domElement.setAttribute("href", reactElement.attributes.href)
+    // domElement.setAttribute("target", reactElement.attributes.target)
+    // container.appendChild(domElement)
+
+    // creating dom element and setting the props dynamically
+
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.child
-    domElement.setAttribute("href", reactElement.attributes.href)
-    domElement.setAttribute("target", reactElement.attributes.target)
+    for (const attribute in reactElement.attributes){
+        domElement.setAttribute(attribute,reactElement.attributes[attribute])
+    }
     container.appendChild(domElement)
 }
 
