deda861a5ab7af6bbe83bc1eaf4f02a5dcdfc984
huzair <huzairahmedkhan@gmail.com>
2024-08-14 12:51:32 +0500

    fix bg changer tailwind issues

--- 00-practice-projects/1.counter/src/App.jsx
+++ 00-practice-projects/1.counter/src/App.jsx
@@ -16,9 +16,9 @@ const subtractValue = ()=>{
    <div className=" text-white grid place-items-center w-full h-screen bg-slate-700">
     <div className="space-y-4 text-center">
    <h1 className="font-semibold text-5xl bg-slate-900 p-5 rounded-xl">Count: {count}</h1>
-   <button onClick={addValue} className="font-medium bg-slate-900  py-2 px-5 rounded-full bg-white text-slate-900 m-5" >Add +</button>
+   <button onClick={addValue} className="font-medium py-2 px-5 rounded-full bg-white text-slate-900 m-5" >Add +</button>
    <br />
-   <button onClick={subtractValue} className="font-medium bg-slate-900  py-2 px-5 rounded-full bg-white text-slate-900 m-5">Subtract -</button>
+   <button onClick={subtractValue} className="font-medium  py-2 px-5 rounded-full bg-white text-slate-900 m-5">Subtract -</button>
    </div>
    </div>
   )
